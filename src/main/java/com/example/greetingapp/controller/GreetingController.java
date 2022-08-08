7a31e692044229ab3694fd729a0a9aadc782eb42
solanki <anujaj2222.gmail.com>
2022-08-09 00:06:17 +0530

    Using Service Layer Geting Greeting Message

--- src/main/java/com/example/greetingapp/controller/GreetingController.java
+++ src/main/java/com/example/greetingapp/controller/GreetingController.java
@@ -1,52 +1,20 @@
 package com.example.greetingapp.controller;
-
-
-import com.example.greetingapp.model.User;
+import com.example.greetingapp.model.Greeting;
+import com.example.greetingapp.services.IGreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetingController {
 
+@Autowired
+    private IGreetingService greetingService;
+
     @RequestMapping(value = "getGreet")
-    public User getString(){
-        User user= new User();
-        user.setFName("Anuj");
-        user.setLName("Solanki");
+    public Greeting getGreeting(){
 
-        return user;
+        return greetingService.addGreeting();
     }
 
-    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
-    public User getString1(@RequestParam(value = "fName") String fName){
-        User user= new User();
-        user.setFName(fName);
-        user.setLName("Solanki");
-        return user;
-    }
 
-    @GetMapping("param/{lName}")
-    public User getString2(@PathVariable String lName){
-        User user=new User();
-        user.setFName("Anuj");
-        user.setLName(lName);
-        return user;
-    }
-
-    @PostMapping("/post")
-    public User getString3(@RequestBody User user){
-        user.setFName("anuj");
-        user.setLName("solanki");
-        return user;
-
-    }
-
-    @PutMapping("/put/{fName}" )
-    public User getString4(@PathVariable String fName,@RequestParam(value = "lName") String lName ){
-
-        User user=new User();
-        user.setFName(fName);
-        user.setLName(lName);
-
-        return user;
-    }
 }
