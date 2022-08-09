9c3306104e8f8c8878e2287c3f1dcb98e9649497
solanki <anujaj2222.gmail.com>
2022-08-09 21:11:11 +0530

    Saving Greeting Message in Repository

--- src/main/java/com/example/greetingapp/services/GreetingService.java
+++ src/main/java/com/example/greetingapp/services/GreetingService.java
@@ -2,20 +2,24 @@ package com.example.greetingapp.services;
 
 import com.example.greetingapp.model.Greeting;
 import com.example.greetingapp.model.User;
+import com.example.greetingapp.repository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class GreetingService implements IGreetingService{
     AtomicLong atomicLong =new AtomicLong();
+    @Autowired
+    private GreetingRepository greetingRepository;
+
     @Override
     public Greeting addGreeting(User user) {
 
         Greeting greeting=new Greeting();
         greeting.setId(atomicLong.incrementAndGet());
         greeting.setMessage("Hello"+" "+user.toString());
-        return greeting;
+        return greetingRepository.save(greeting);
     }
 
     @Override
